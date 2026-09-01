9b2b521e3f051b1e798c8076406f23c3a45d7bdf
Borys Szefczyk <boryszef@gmail.com>
2026-09-01 19:36:03 +0200

    separate game logic from UI

---
+++ src/game.rs
@@ -0,0 +1,146 @@
+use rand::prelude::IndexedRandom;
+use std::fmt;
+
+const MAP_MIN_SIZE: u32 = 3;
+const MAP_PER_LEVEL_INCREMENT: u32 = 2;
+
+#[derive(Debug)]
+pub struct Player {
+    level: u32,
+    coordinates: (i32, i32),
+    resources: Vec<Resource>,
+}
+
+impl Default for Player {
+    fn default() -> Player {
+        Player {
+            level: 1,
+            coordinates: (0, 0),
+            resources: Vec::new(),
+        }
+    }
+}
+
+impl Player {
+    pub fn walk_west(&mut self, boundaries: (i32, i32, i32, i32)) {
+        if self.coordinates.0 > boundaries.0 {
+            self.coordinates.0 -= 1;
+        }
+    }
+
+    pub fn walk_east(&mut self, boundaries: (i32, i32, i32, i32)) {
+        if self.coordinates.0 < boundaries.1 {
+            self.coordinates.0 += 1;
+        }
+    }
+
+    pub fn walk_north(&mut self, boundaries: (i32, i32, i32, i32)) {
+        if self.coordinates.1 < boundaries.2 {
+            self.coordinates.1 += 1;
+        }
+    }
+
+    pub fn walk_south(&mut self, boundaries: (i32, i32, i32, i32)) {
+        if self.coordinates.1 > boundaries.3 {
+            self.coordinates.1 -= 1;
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Resource {
+    Wood,
+}
+
+#[derive(Clone, Copy)]
+enum TerrainType {
+    Meadow,
+    Forest,
+    Village,
+    Deadland,
+}
+
+impl fmt::Debug for TerrainType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let symbol = match self {
+            TerrainType::Meadow => '𖧧',
+            TerrainType::Forest => '𖠰',
+            TerrainType::Village => '🛖',
+            TerrainType::Deadland => ' ',
+        };
+        write!(f, "{symbol}")
+    }
+}
+
+const TERRAIN_TYPES: &[TerrainType] = &[
+    TerrainType::Meadow,
+    TerrainType::Forest,
+    TerrainType::Deadland,
+];
+
+struct MapTile {
+    terrain_type: TerrainType,
+    resources: Vec<Resource>,
+}
+
+impl fmt::Debug for MapTile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let terrain = self.terrain_type;
+        write!(f, "{:?}", terrain)
+    }
+}
+
+impl MapTile {
+    fn new() -> MapTile {
+        let mut rng = rand::rng();
+        let terrain_type = *TERRAIN_TYPES.choose(&mut rng).unwrap();
+
+        MapTile {
+            terrain_type,
+            resources: Vec::new(),
+        }
+    }
+
+    fn generate(terrain_type: TerrainType) -> MapTile {
+        MapTile {
+            terrain_type,
+            resources: Vec::new(),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RegionMap {
+    tiles: Vec<Vec<MapTile>>,
+    size: (u32, u32),
+    pub boundaries: (i32, i32, i32, i32), // (-max_x, max_x, -max_y, max_y)
+}
+
+impl RegionMap {
+    pub fn new(player: &Player) -> RegionMap {
+        let size = MAP_MIN_SIZE + player.level * MAP_PER_LEVEL_INCREMENT;
+        let mut map = Vec::new();
+        let middle = (size / 2, size / 2);
+        for x in 0..size {
+            let mut row = Vec::new();
+            for y in 0..size {
+                let tile = match (x, y) {
+                    pos if pos == middle => MapTile::generate(TerrainType::Village),
+                    _ => MapTile::new(),
+                };
+                row.push(tile);
+            }
+            map.push(row);
+        }
+        RegionMap {
+            tiles: map,
+            size: (size, size),
+            boundaries: (
+                -(middle.0 as i32),
+                middle.1 as i32,
+                middle.0 as i32,
+                -(middle.1 as i32),
+            ),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+mod game;
+
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use rand::prelude::IndexedRandom;
+use game::{Player, RegionMap};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
@@ -9,142 +11,27 @@ use ratatui::{
     text::{Line, Text},
     widgets::{Block, Paragraph, Widget},
 };
-use std::fmt;
 use std::io;
 
-const MAP_MIN_SIZE: u32 = 5;
-const MAP_PER_LEVEL_INCREMENT: u32 = 2;
-
 #[derive(Debug)]
-struct Player {
-    level: u32,
-    coordinates: (u32, u32),
-    resources: Vec<Resource>,
-}
-
-impl Default for Player {
-    fn default() -> Player {
-        Player {
-            level: 1,
-            coordinates: (0, 0),
-            resources: Vec::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Resource {
-    Wood,
-}
-
-#[derive(Clone, Copy, Debug)]
-enum TerrainType {
-    Meadow,
-    Forest,
-    Village,
-    Deadland,
-}
-
-impl fmt::Display for TerrainType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let symbol = match self {
-            TerrainType::Meadow => '𖧧',
-            TerrainType::Forest => '𖠰',
-            TerrainType::Village => '🛖',
-            TerrainType::Deadland => ' ',
-        };
-        write!(f, "{symbol}")
-    }
-}
-
-const PLAYER: char = '𐦂';
-
-const TERRAIN_TYPES: &[TerrainType] = &[
-    TerrainType::Meadow,
-    TerrainType::Forest,
-    TerrainType::Deadland,
-];
-
-#[derive(Debug)]
-struct MapTile {
-    terrain_type: TerrainType,
-    resources: Vec<Resource>,
-}
-
-impl fmt::Display for MapTile {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let symbol = self.terrain_type.to_string();
-        write!(f, "{symbol}")
-    }
-}
-
-impl MapTile {
-    fn new() -> MapTile {
-        let mut rng = rand::rng();
-        let terrain_type = *TERRAIN_TYPES.choose(&mut rng).unwrap();
-
-        MapTile {
-            terrain_type,
-            resources: Vec::new(),
-        }
-    }
-
-    fn generate(terrain_type: TerrainType) -> MapTile {
-        MapTile {
-            terrain_type,
-            resources: Vec::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct RegionMap {
-    tiles: Vec<Vec<MapTile>>,
-    size: (u32, u32),
-}
-
-impl fmt::Display for RegionMap {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in &self.tiles {
-            write!(f, "|")?;
-            for tile in row {
-                write!(f, "{tile}")?;
-            }
-            writeln!(f, "|")?;
-        }
-
-        Ok(())
-    }
-}
-
-impl RegionMap {
-    fn new(player: &Player) -> RegionMap {
-        let size = MAP_MIN_SIZE + player.level * MAP_PER_LEVEL_INCREMENT;
-        let mut map = Vec::new();
-        let middle = (size / 2, size / 2);
-        for x in 0..size {
-            let mut row = Vec::new();
-            for y in 0..size {
-                let tile = match (x, y) {
-                    pos if pos == middle => MapTile::generate(TerrainType::Village),
-                    _ => MapTile::new(),
-                };
-                row.push(tile);
-            }
-            map.push(row);
-        }
-        RegionMap {
-            tiles: map,
-            size: (size, size),
-        }
-    }
-}
-
-#[derive(Debug, Default)]
 pub struct App {
     player: Player,
+    map: RegionMap,
     counter: u8,
     exit: bool,
+}
+
+impl Default for App {
+    fn default() -> App {
+        let player = Player::default();
+        let map = RegionMap::new(&player);
+        App {
+            player,
+            map,
+            counter: 0,
+            exit: false,
+        }
+    }
 }
 
 impl App {
@@ -169,8 +56,10 @@ impl App {
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
+            KeyCode::Left => self.walk_west(),
+            KeyCode::Right => self.walk_east(),
+            KeyCode::Up => self.walk_north(),
+            KeyCode::Down => self.walk_south(),
             _ => {}
         }
     }
@@ -179,12 +68,20 @@ impl App {
         self.exit = true;
     }
 
-    fn increment_counter(&mut self) {
-        self.counter += 1;
+    fn walk_west(&mut self) {
+        self.player.walk_west(self.map.boundaries);
     }
 
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
+    fn walk_east(&mut self) {
+        self.player.walk_east(self.map.boundaries);
+    }
+
+    fn walk_north(&mut self) {
+        self.player.walk_north(self.map.boundaries);
+    }
+
+    fn walk_south(&mut self) {
+        self.player.walk_south(self.map.boundaries);
     }
 
     fn draw(&self, frame: &mut Frame) {
@@ -196,11 +93,12 @@ impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(" Counter App Tutorial ".bold());
         let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
+            " Move: ".into(),
+            "<Left> ".blue().bold(),
+            "<Right> ".blue().bold(),
+            "<Up> ".blue().bold(),
+            "<Down>".blue().bold(),
+            " Quit: ".into(),
             "<Q> ".blue().bold(),
         ]);
         let block = Block::bordered()
@@ -209,10 +107,12 @@ impl Widget for &App {
             .border_set(border::THICK);
 
         let player = &self.player;
+        let map = &self.map;
 
         let counter_text = Text::from(vec![
             Line::from(vec!["Value: ".into(), self.counter.to_string().yellow()]),
             Line::from(vec![format!("{player:?}").into()]),
+            Line::from(vec![format!("{map:?}").into()]),
         ]);
 
         Paragraph::new(counter_text)
@@ -224,20 +124,4 @@ impl Widget for &App {
 
 fn main() -> io::Result<()> {
     ratatui::run(|terminal| App::default().run(terminal))
-}
-
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
-        }
-    }
-}
-
-fn render(frame: &mut Frame) {
-    let player = Player::default();
-    let map = RegionMap::new(&player);
-    let output = String::from("{player:?}");
-    frame.render_widget(output, frame.area());
 }
